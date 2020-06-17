ce0b781dd615d283040b2b8e9a6d48ef46dc06c1
ezrablack <timothydanielmorales@gmail.com>
2020-06-16 19:30:06 -0500

    slight css added

--- client/src/components/Layout.js
+++ client/src/components/Layout.js
@@ -6,8 +6,29 @@ export default function Layout (){
 
 
     return(
-        <div>
-            
-        </div>
+        <div class="ui internally celled grid">
+  <div class="row">
+    <div class="doubling three wide column">
+      <img></img>
+    </div>
+    <div class="five high ten wide column">
+      <p></p>
+    </div>
+    <div class="three wide column">
+      <img></img>
+    </div>
+  </div>
+  <div class="row">
+    <div class="three wide column">
+      <img></img>
+    </div>
+    <div class="ten wide column">
+      <p></p>
+    </div>
+    <div class="three wide column">
+      <img></img>
+    </div>
+  </div>
+</div>
     )
 }
