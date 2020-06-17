1006d501cdc1f0ca2186126e2765589ba73a05c6
ezrablack <timothydanielmorales@gmail.com>
2020-06-17 17:46:26 -0500

    project complete - updated

--- client/src/components/TextContainer/TextContainer.js
+++ client/src/components/TextContainer/TextContainer.js
@@ -10,7 +10,7 @@ const TextContainer = ({ users }) => (
       users
         ? (
           <div>
-            <h1>People currently chatting:</h1>
+            <h1>Currently Online:</h1>
             <div className="activeContainer">
               <h2>
                 {users.map(({name}) => (
